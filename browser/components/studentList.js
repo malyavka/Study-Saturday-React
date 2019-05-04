f6c313b13f8fecf6c4720de6cafc2c6b6226d595
Maria Obaturova <m.obaturova@gmail.com>
2019-05-04 11:37:42 -0400

    smth

--- browser/components/studentList.js
+++ browser/components/studentList.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios'
+import axios from 'axios';
 
+
+export default class Main extends React.component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            students: []
+        }
+    }
+
+    async componentDidMount () {
+        try {
+            const response = await axios.get('/student')
+            const students = response.data; // data is right there! No `await response.json()` nonsense!
+            console.log('Got students: ', students)
+            this.setState({students})
+
+        } catch (err) {
+            console.log('There was a problem getting students!')
+        }
+    }
+    render () {
+        return(
+            <div id='main'>
+                <h1>STUDENTS</h1>
+                <div id='container'>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <th>Name {this.state.students[0]}</th>
+                            <th>Name</th>
+                            <th>Name</th>
+                            <th>Name</th>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        )
+    }
+}
 
 
 
--- browser/index.js
+++ browser/index.js
@@ -6,31 +6,3 @@ import ReactDOM from 'react-dom';
 import Students from './components/studentList';
 
 
-
-export default class Main extends React.component {
-    constructor () {
-        super()
-        this.state = {
-            students: []
-        }
-    }
-    async componentDidNount () {
-        try {
-            const response = await axios.get('/student')
-            const students = response.data; // data is right there! No `await response.json()` nonsense!
-            console.log('Got students: ', students)
-        } catch (err) {
-            console.log('There was a problem getting students!')
-        }
-    }
-    render () {
-        return(
-            <div id='main'>
-                <h1>STUDENTS</h1>
-                <div id='container'>
-                    <Students students = {this.state.students} />
-                </div>
-            </div>
-        )
-    }
-}
